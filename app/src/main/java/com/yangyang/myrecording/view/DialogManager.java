8f887b61a573624a3993d8c2ec1808609879de67
yangyang <1075182781@qq.com>
2016-05-08 23:00:16 +0800

    增加了DialogManager

---
+++ app/src/main/java/com/yangyang/myrecording/view/DialogManager.java
@@ -0,0 +1,90 @@
+package com.yangyang.myrecording.view;
+
+
+import android.app.Dialog;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.yangyang.myrecording.R;
+
+/**
+ * Created by asus on 2016/5/8.
+ * 用于管理Dialog的变化
+ */
+public class DialogManager {
+    private Dialog mDialog;
+    private ImageView mRecorder;
+    private ImageView mVoice;
+    private TextView mLabel;
+    private Context context;
+
+    public DialogManager(Context context) {
+        this.context = context;
+    }
+    public void showRecordingDialog(){
+        mDialog=new Dialog(context, R.style.Theme_AudioDialog);
+        LayoutInflater inflater=LayoutInflater.from(context);
+        View view=inflater.inflate(R.layout.dialog_recorder,null);
+        mDialog.setContentView(view);
+        mLabel = (TextView) mDialog.findViewById(R.id.id_label);
+        mRecorder= (ImageView) mDialog.findViewById(R.id.id_recorder);
+        mVoice= (ImageView) mDialog.findViewById(R.id.id_voice);
+        mDialog.show();
+
+        //展示默认的对话框
+    }
+    public void Recording(){
+        if(mDialog!=null&&mDialog.isShowing()){
+            mLabel.setVisibility(View.VISIBLE);
+            mVoice.setVisibility(View.VISIBLE);
+            mRecorder.setVisibility(View.VISIBLE);
+            mRecorder.setImageResource(R.mipmap.recorder);
+            mLabel.setText(R.string.hint_recording);
+        }
+    }
+    public void wantToCancel(){
+        //取消录音
+        if(mDialog!=null&&mDialog.isShowing()){
+            mLabel.setVisibility(View.VISIBLE);
+            mVoice.setVisibility(View.GONE);
+            mRecorder.setVisibility(View.VISIBLE);
+            mRecorder.setImageResource(R.mipmap.cancel);
+            mLabel.setText(R.string.hint_cancel);
+        }
+    }
+    public void TooShort(){
+        //时间太短
+        if(mDialog!=null&&mDialog.isShowing()){
+            mLabel.setVisibility(View.VISIBLE);
+            mVoice.setVisibility(View.GONE);
+            mRecorder.setVisibility(View.VISIBLE);
+            mRecorder.setImageResource(R.mipmap.voice_to_short);
+            mLabel.setText(R.string.hint_timeshort);
+        }
+    }
+    public void dismissDialog(){
+        //隐藏对话框
+        if(mDialog!=null&&mDialog.isShowing()){
+            mDialog.dismiss();
+            mDialog=null;
+        }
+    }
+
+    /**
+     * 更新图片，level为1——7
+     * @param level
+     */
+    public void UpdateVoiceLevel(int level){
+        //更新声音
+        if(mDialog!=null&&mDialog.isShowing()){
+            mLabel.setVisibility(View.VISIBLE);
+            mVoice.setVisibility(View.VISIBLE);
+            mRecorder.setVisibility(View.VISIBLE);
+            int resID=context.getResources().getIdentifier("v"+level,"drawable",context.getPackageName());
+            mVoice.setImageResource(resID);
+        }
+    }
+}
--- app/src/main/java/com/yangyang/myrecording/view/MyRecordingButton.java
+++ app/src/main/java/com/yangyang/myrecording/view/MyRecordingButton.java
@@ -5,16 +5,19 @@ import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.widget.Button;
 
+import com.yangyang.myrecording.R;
+
 /**
  * Created by asus on 2016/5/8.
  * 自定义录音按钮，有三种状态
  */
 public class MyRecordingButton extends Button{
+    private static final int DISTANCE_Y_CANCEL=50;//这里应该转换dp和px
     private static final int STATE_NORMAL=1;
     private static final int STATE_RECORDING=2;
     private static final int STATE_WANTCANCEL=3;
     private int mCueState=STATE_NORMAL;
-    public boolean isRecording=false;
+    private boolean isRecording=false;
 
     public MyRecordingButton(Context context) {
         this(context, null);
@@ -31,6 +34,8 @@ public class MyRecordingButton extends Button{
         int y= (int) event.getY();
         switch (action) {
             case MotionEvent.ACTION_DOWN:
+                //TODO
+                isRecording=true;
                 ChangeState(STATE_RECORDING);
                 break;
 
@@ -71,10 +76,39 @@ public class MyRecordingButton extends Button{
     }
 
     private boolean wantCancel(int x, int y) {
+        if(x<0||x>getWidth()){
+            //超出button的范围
+            return true;
+        }
+        if(y<-DISTANCE_Y_CANCEL||y>getHeight()+DISTANCE_Y_CANCEL){
+            return true;
+        }
         return false;
     }
 
-    private void ChangeState(int stateRecording) {
+    private void ChangeState(int state) {
+        if(mCueState!=state){
+            mCueState=state;
+            switch (state){
+                case STATE_NORMAL:
+                    setBackgroundResource(R.drawable.btn_normal);
+                    setText(R.string.str_btn_recording);
+                    break;
+                case STATE_RECORDING:
+                    setBackgroundResource(R.drawable.btn_recording);
+                    setText(R.string.str_btn_send);
+                    if(isRecording){
+                        //TODO 更新Dialog.recording()
+                    }
+                    break;
+                case STATE_WANTCANCEL:
+                    setBackgroundResource(R.drawable.btn_recording);
+                    setText(R.string.str_btn_cancel);
+                    //TODO 更新Dialog.WantCancel()
+                    break;
+
+            }
+        }
 
     }
 
